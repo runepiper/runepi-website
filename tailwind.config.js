a36cbbeb5761055a49c59810f682d76dd8757ba3
Rune Piper <kontakt@runepiper.de>
2024-05-22 10:36:27 +0200

    chore: test build-fe job

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
           700: '#484848'
         },
         yellow: {
-          500: '#ef7d1b'
+          500: '#ff0000'
         },
         black: {
           DEFAULT: '#333333'
